bd916892ca0a8139230761e7c298f07b77b9fe0c
Juan Jose M <juanjosemarinarias9@gmail.com>
2024-05-16 23:32:30 -0500

    registro y login

--- Gestion/www/js/index.js
+++ Gestion/www/js/index.js
@@ -1,49 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
-
-document.addEventListener('init', function(event) {
-
-    var page = event.target;
-  
-    if (page.id === 'page1') {
-      page.querySelector('#push-button').onclick = function() {
-        console.log("Click en el boton!!! con paso de parámetros..");
-        document.querySelector('#myNavigator').pushPage('page2.html', 
-        {data: {title: 'Page 2', myData: JSON.stringify({"first":"primer dato"})}});
-      };
+function login() {
+      // Redirigir a la página de inicio de sesión
+      window.location.href = '../principal.html'; // Cambia 'login.html' a la URL de tu página de inicio de sesión
     }
-    else if (page.id === 'page2') {
-      console.log(page.data);
-      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+
+function register() {
+      // Redirigir a la página de registro
+      window.location.href = '../registro.html'; // Cambia 'register.html' a la URL de tu página de registro
     }
-    else if (page.id === 'page3') {
-      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+ function registrar() {
+      // Mostrar mensaje de registro exitoso
+      document.getElementById('registered-message').style.display = 'block';
+      // Redirigir a otra página después de 2 segundos (2000 milisegundos)
+      setTimeout(function() {
+        window.location.href = '../index.html'; // Cambia 'bienvenido.html' a la URL de tu página de bienvenida
+      }, 2000);
     }
-});
+
